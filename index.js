7acafdc19300a0c0c7e8d78e205b98fd18fc1c69
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-06-11 12:50:06 +0600

    instructor page updated

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+
+    //role instructor
+    app.get('/instructorUsers', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
